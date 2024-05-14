1884593e01bb26507ae0f6718bbe6cd0b83eab39
zaiheshi <zaiheshi@gmail.com>
2024-05-14 14:18:16 +0800

    remove xserver

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "Zaiheshi's Configuration for NixOS";
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-stable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     agenix.url = "github:ryantm/agenix";
     home-manager.url = "github:nix-community/home-manager";
