9b27aa914a910f0e3d6d6deb47c73c8f3cb388a4
zaiheshi <zaiheshi@gmail.com>
2024-05-14 19:59:33 +0800

    update vim config

--- templates/starter-with-secrets/modules/shared/home-manager.nix
+++ templates/starter-with-secrets/modules/shared/home-manager.nix
@@ -10,25 +10,6 @@ let name = "%NAME%";
     enableCompletion = true;
     # autosuggestions.enable = true;
     # syntaxHighlighting.enable = true;
-    autocd = false;
-    cdpath = [ "~/.local/share/src" ];
-    initExtraFirst = ''
-      if [[ -f /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh ]]; then
-        . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
-        . /nix/var/nix/profiles/default/etc/profile.d/nix.sh
-      fi
-
-      # Define variables for directories
-      export PATH=$HOME/.local/share/bin:$PATH
-
-      # Remove history data we don't want to see
-      export HISTIGNORE="pwd:ls:cd"
-
-      export EDITOR="vim"
-
-      # Always color ls and group directories
-      alias ls='ls --color=auto'
-    '';
   };
 
   git = {
@@ -53,111 +34,33 @@ let name = "%NAME%";
 
   vim = {
     enable = true;
-    plugins = with pkgs.vimPlugins; [ vim-airline vim-airline-themes vim-startify vim-tmux-navigator ];
-    settings = { ignorecase = true; };
     extraConfig = ''
-      "" General
-      set number
-      set history=1000
-      set nocompatible
-      set modelines=0
+      let mapleader=" "
+      colorscheme desert
       set encoding=utf-8
-      set scrolloff=3
-      set showmode
-      set showcmd
-      set hidden
-      set wildmenu
-      set wildmode=list:longest
-      set cursorline
-      set ttyfast
-      set nowrap
-      set ruler
-      set backspace=indent,eol,start
-      set laststatus=2
-      set clipboard=autoselect
+      set mouse=a
 
-      " Dir stuff
+      set nocompatible
       set nobackup
-      set nowritebackup
       set noswapfile
-      set backupdir=~/.config/vim/backups
-      set directory=~/.config/vim/swap
+      set ignorecase
+      set textwidth=80
 
-      " Relative line numbers for easy movement
-      set relativenumber
-      set rnu
+      filetype plugin on
+      syntax on
 
-      "" Whitespace rules
-      set tabstop=8
-      set shiftwidth=2
-      set softtabstop=2
+      " 解决insert模式下面退格键不能用的问题
+      set backspace=indent,eol,start
+
+      set nu
+      " set relativenumber
+      set ts=4
+      set shiftwidth=4
       set expandtab
 
-      "" Searching
-      set incsearch
-      set gdefault
-
-      "" Statusbar
-      set nocompatible " Disable vi-compatibility
-      set laststatus=2 " Always show the statusline
-      let g:airline_theme='bubblegum'
-      let g:airline_powerline_fonts = 1
-
-      "" Local keys and such
-      let mapleader=","
-      let maplocalleader=" "
-
-      "" Change cursor on mode
-      :autocmd InsertEnter * set cul
-      :autocmd InsertLeave * set nocul
-
-      "" File-type highlighting and configuration
-      syntax on
-      filetype on
-      filetype plugin on
-      filetype indent on
-
-      "" Paste from clipboard
-      nnoremap <Leader>, "+gP
-
-      "" Copy from clipboard
-      xnoremap <Leader>. "+y
-
-      "" Move cursor by display lines when wrapping
+      nnoremap <leader>w :w<CR>
+      nnoremap <leader>q :q<CR>
       nnoremap j gj
-      nnoremap k gk
-
-      "" Map leader-q to quit out of window
-      nnoremap <leader>q :q<cr>
-
-      "" Move around split
-      nnoremap <C-h> <C-w>h
-      nnoremap <C-j> <C-w>j
-      nnoremap <C-k> <C-w>k
-      nnoremap <C-l> <C-w>l
-
-      "" Easier to yank entire line
-      nnoremap Y y$
-
-      "" Move buffers
-      nnoremap <tab> :bnext<cr>
-      nnoremap <S-tab> :bprev<cr>
-
-      "" Like a boss, sudo AFTER opening the file to write
-      cmap w!! w !sudo tee % >/dev/null
-
-      let g:startify_lists = [
-        \ { 'type': 'dir',       'header': ['   Current Directory '. getcwd()] },
-        \ { 'type': 'sessions',  'header': ['   Sessions']       },
-        \ { 'type': 'bookmarks', 'header': ['   Bookmarks']      }
-        \ ]
-
-      let g:startify_bookmarks = [
-        \ '~/.local/share/src',
-        \ ]
-
-      let g:airline_theme='bubblegum'
-      let g:airline_powerline_fonts = 1
       '';
      };
 
